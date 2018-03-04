7ca9787e8f9a4354cf62083c1f768df9ed020fb3
Petr Pavlik <petrpavlik@me.com>
2018-03-04 13:02:07 +0100

    fix swiftlint-danger repo URL

--- danger/Dangerfile.swift
+++ danger/Dangerfile.swift
@@ -1,7 +1,7 @@
 // Dangerfile.swift
 
 import Danger
-import DangerSwiftLint // package: https://github.com/ashfurrow/DangerSwiftLint.git
+import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
 
 SwiftLint.lint()
 
