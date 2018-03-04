78ae40385ba1bff3399737b191b0eae093d0cf14
Petr Pavlik <petrpavlik@me.com>
2018-03-04 13:29:50 +0100

    debugging

--- danger/Dangerfile.swift
+++ danger/Dangerfile.swift
@@ -3,6 +3,8 @@
 import Danger
 import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
 
+var x = 5; // to generate a linter warning
+
 SwiftLint.lint()
 
 message("It works!")
